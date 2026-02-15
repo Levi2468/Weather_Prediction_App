4d5b28ff933aa53ff97355638df1ea014fe17c41
Vishnu parameswaran <99706618+Levi2468@users.noreply.github.com>
2026-02-15 18:27:07 +0530

    Update settings.py

--- weather_ML/settings.py
+++ weather_ML/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bhmdiis)gye&=w%$7t)y7!k_2ditdv@pjya15&=wxv%zodkiti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.20.79.122']
 
 
 # Application definition
